2851fc1ece8526980dbc7f5a3ff272563129e9a8
Leo Li <2907517155@qq.com>
2026-04-13 20:34:56 +0800

    Commit soil_sensor_email.py

---
+++ soil_sensor_email.py
@@ -0,0 +1,115 @@
+# AUTHOR: Leo Li
+# DATE: 2026/4/13
+
+# ---- 1. Import Libraries ----
+
+# Email Part
+import smtplib
+from email.message import EmailMessage
+
+# Soil Sensor Part
+import RPi.GPIO as GPIO
+import time
+import schedule
+
+
+# ---- 2. Function Definition ----
+
+# Callback function to be called when the sensor detects a change
+def callback(channel):
+    if GPIO.input(channel):
+        subject = "Soil Status Report: Wet"
+        body = """The soil is currently wet.
+        No watering needed."""
+        send_email(subject, body)
+
+
+# Function to set up the email server
+def set_server(from_email, password):
+    # Email server and port
+    SMTP_SERVER = "smtp.qq.com"
+    SMTP_PORT = 587
+
+    # Set the email server and port
+    server = smtplib.SMTP(SMTP_SERVER, SMTP_PORT)
+
+    # Start TLS for security
+    server.starttls()
+
+    # Login to the SMTP server
+    print("<Logging in to the email server...>")
+    server.login(from_email, password)
+
+    return server
+
+
+# Function to send email
+def send_email(subject, body):
+    global from_email, password, to_email
+    # Set up the email server
+    server = set_server(from_email, password)
+
+    # Create a message object
+    msg = EmailMessage()
+        
+    # Set the email body
+    msg.set_content(body)
+        
+    # Set sender and recipient 
+    msg["From"] = from_email
+    msg["To"] = to_email
+        
+    # Set the email subject
+    msg["Subject"] = subject
+    
+    # Send the message
+    server.send_message(msg)
+    print("<Email sent successfully>")
+
+    # Disconnect from the server
+    server.quit()
+
+
+# Function to report status everyday at regular intervals
+def job():
+    global channel
+    is_wet = GPIO.input(channel)
+    if is_wet:
+        subject = "Soil Status Report: Wet"
+        body = """The soil is currently wet.
+        No watering needed."""
+    else:
+        subject = "Soil Status Report: Dry"
+        body = """The soil is currently dry.
+        Watering is needed."""
+
+    send_email(subject, body)
+
+
+# ---- 4. Configure Soil Sensor ----
+
+# Set the GPIO mode
+GPIO.setmode(GPIO.BCM)
+
+# Set pin 4 as input to read data from the sensor
+channel = 4
+GPIO.setup(channel, GPIO.IN)
+
+# ---- 5. The Main Program ----
+from_email = "1520087861@qq.com"
+password = "xnwpqldbsshzifdg"
+to_email = "2907517155@qq.com"
+
+# Set the job to be done twice a day
+schedule.every().day.at("10:00").do(job)
+schedule.every().day.at("18:00").do(job)
+
+# Event detection
+# Let us know when the pin goes HIGH or LOW
+GPIO.add_event_detect(channel, GPIO.BOTH, bouncetime = 300)
+# Assign function to GPIO PIN, Run function on change
+GPIO.add_event_callback(channel, callback)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
